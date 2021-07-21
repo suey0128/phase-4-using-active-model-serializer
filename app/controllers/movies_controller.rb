78b54222f4a1c8730a27d301a79775148063cf85
suey0128 <syui0128@gmail.com>
2021-07-21 09:14:05 -0700

    commit tree 4805d9c8067d0f056fbbf1a9d908415270a1ecf3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,7 +8,20 @@ class MoviesController < ApplicationController
 
   def show
     movie = Movie.find(params[:id])
+    # render json: movie.to_json
+    #   (only: [:id, :title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director])
+    # render json: movie.to_json(except: [:created_at, :updated_at])
     render json: movie
+  end
+
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end
+
+  def summaries
+    movies = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer
   end
 
   private
